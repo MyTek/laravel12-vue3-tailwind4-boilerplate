b328e1892c31ee7acce853ac3954d0ac32a356a5
John Dye <john@mytek.la>
2026-02-12 09:57:18 -0800

    More cors issues

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,14 +3,10 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-
-    // Do NOT use allowed_origins ['*'] when supports_credentials is true.
-    // Use patterns so Laravel reflects the requesting Origin.
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ['*'],
-
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
     'max_age' => 0,
-//    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
